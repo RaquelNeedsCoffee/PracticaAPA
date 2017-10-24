c4507d13bf87da077aa77322bebca21c48962470
adrian729 <adrian.sanchez.albanell@gmail.com>
2017-10-24 20:51:16 +0200

    update get data

--- Code/gettingData.py
+++ Code/gettingData.py
@@ -1,17 +1,17 @@
 import requests
 
-opendota = requests.get('https://api.opendota.com/api/proMatches')
-sorted_pro_matches = sorted(opendota.json(), key=lambda k: k['match_id'])
-print(sorted_pro_matches[0]['match_id'])
 
-sorted_pro_matches = []
-parameters = {}
+def get_data():
+    sorted_pro_matches = []
+    parameters = {}
+    num_reqs = 5
+    for i in range(0, num_reqs):
+        if len(sorted_pro_matches) > 0:
+            parameters = {'less_than_match_id': sorted_pro_matches[0]['match_id']}
+        open_dota = requests.get('https://api.opendota.com/api/proMatches', params=parameters)
+        sbm = sorted(open_dota.json(), key=lambda k: k['match_id'])
+        sorted_pro_matches = sbm + sorted_pro_matches
+        print(sorted_pro_matches[0]['match_id'], sorted_pro_matches[-1]['match_id'], len(sorted_pro_matches))
 
-num_reqs = 5
-for i in range(0, num_reqs):
-    if len(sorted_pro_matches) > 0:
-        parameters = {'less_than_match_id': sorted_pro_matches[0]['match_id']}
-    opendota = requests.get('https://api.opendota.com/api/proMatches', params=parameters)
-    sbm = sorted(opendota.json(), key=lambda k: k['match_id'])
-    sorted_pro_matches = sbm + sorted_pro_matches
-    print(sorted_pro_matches[0]['match_id'], sorted_pro_matches[-1]['match_id'], len(sorted_pro_matches))
+
+get_data()
