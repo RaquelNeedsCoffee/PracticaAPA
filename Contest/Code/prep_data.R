6e4d8d73939ccf38f5ef5dfbeda8ea220df5f50a
adrian729 <adrian.sanchez.albanell@gmail.com>
2017-11-21 11:36:43 +0100

    fread fixed local

--- Contest/Code/prep_data.R
+++ Contest/Code/prep_data.R
@@ -1,9 +1,9 @@
 library(data.table)
 
 ## read data tables
-members_dt <- fread(paste(dir, "Contest/Data/members.csv", sep=""))
-songs_dt <- fread(paste(dir, "Contest/Data/songs.csv", sep=""))
-train_dt <- fread(paste(dir, "Contest/Data/train.csv", sep=""))
-test_dt <- fread(paste(dir, "Contest/Data/test.csv", sep=""))
+members_dt <- fread("Contest/Data/members.csv")
+songs_dt <- fread("Contest/Data/songs.csv")
+train_dt <- fread("Contest/Data/train.csv")
+test_dt <- fread("Contest/Data/test.csv")
 
 save(members_dt, songs_dt, train_dt, test_dt, file="Contest/Data/data_tables.RData")
