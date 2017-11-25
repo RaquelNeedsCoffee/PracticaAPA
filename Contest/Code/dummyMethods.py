94aa0232886e0396ba63fc0025ecfaddb86e140f
Unknown <raquelpa93@gmail.com>
2017-11-25 13:39:33 +0100

    actualizo la svm y meto el lda

--- Contest/Code/dummyMethods.py
+++ Contest/Code/dummyMethods.py
@@ -9,15 +9,46 @@ from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from os import path
 from Contest.Code.split_data import split
+from sklearn.linear_model import LogisticRegression
 import pickle
 import gc
+
+
+def lda(X_train, X_test, y_train, y_test):
+    """
+    La regresión logística de toda la vida.
+    La idea es hacer un clasificador lineal y pasarlo por la función logística.
+    P(C|x) = g(wTx+w0)
+    Suposiciones:
+    - Gausianidad de los datos
+    - La misma varianza
+    :param X_train:
+    :param X_test:
+    :param y_train:
+    :param y_test:
+    :return:
+    """
+    print("Regresion Lineal:")
+    # Importante estandarizar datos
+    lda = LogisticRegression(solver='saga', n_jobs=-1)
+    _path = '../Data/Models/lda.plk'
+    if path.isfile(_path):
+        lda_trained = pickle.load(open(_path, 'rb'))
+    else:
+        print('Training the model...')
+        lda_trained = lda.fit(X_train, y_train.values.ravel())
+        with open(_path, 'wb') as handle:
+            pickle.dump(lda_trained, handle)
+    pred = lda_trained.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, pred))
+    print('Full report: \n', metrics.classification_report(y_test, pred))
 
 
 def naive_bayes(X_train, X_test, y_train, y_test):
     print('Naive Bayes: ')
     clf = GaussianNB()
     if path.isfile('../Data/Models/nb'):
-        clf_trained = pickle.load(open('Data/Models/nb', 'rb'))
+        clf_trained = pickle.load(open('../Data/Models/nb', 'rb'))
     else:
         print('Training the model...')
         clf_trained = clf.fit(X_train, y_train.values.ravel())
@@ -50,7 +81,7 @@ def knn(X_train, X_test, y_train, y_test):
 
 def random_forest(X_train, X_test, y_train, y_test, n_estimators):
     print('Random Forest: ')
-    rf_path = '../Data/Models/rf'+str(n_estimators)+'.pkl'
+    rf_path = '../Data/Models/rf' + str(n_estimators) + '.pkl'
     rf = RandomForestClassifier(n_estimators=n_estimators)
     if path.isfile(rf_path):
         rf_trained = pickle.load(open(rf_path, 'rb'))
@@ -67,10 +98,14 @@ def random_forest(X_train, X_test, y_train, y_test, n_estimators):
     gc.collect()
 
 
-def main():
-    (X_train, X_test, X_val, y_train, y_test, y_val) = split(0.95)
+def run_rf(X_train, X_test, X_val, y_train, y_test, y_val):
     for i in [1, 3, 5, 7, 9, 11, 13, 15, 17]:
         random_forest(X_train, X_test, y_train, y_test, i)
+
+
+def main():
+    (X_train, X_test, X_val, y_train, y_test, y_val) = split(0.95)
+    lda(X_train, X_test, y_train, y_test)
 
 
 if __name__ == "__main__":
--- Contest/Code/svm.py
+++ Contest/Code/svm.py
@@ -7,9 +7,9 @@ import sklearn.neighbors as nb  # knn
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 from sklearn.preprocessing import MinMaxScaler
-
+from os import path
 from sklearn.svm import SVC
-
+import _pickle as pickle
 
 from Contest.Code.split_data import split
 
@@ -20,19 +20,23 @@ def svm():
     :return:
     """
     print('We are spliting the data: ')
-    (X_train, X_test, X_val, y_train, y_test, y_val) = split(0.95)
+    (X_train, X_test, X_val, y_train, y_test, y_val) = split(0.999)
     # We scalate the data for using svm
     scaler = MinMaxScaler(feature_range=(-1, 1)).fit(X_train)
 
     # Apply the normalization trained in training data in both training and test sets
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
-
     print('linear SVM: ')
     knc = SVC(kernel='linear')
-    print('Training...')
-    knc.fit(X_train, y_train.values.ravel())
-    pred=knc.predict(X_test)
+    if path.isfile('../Data/Models/svm.pkl'):
+        svm_trained = pickle.load(open('../Data/Models/svm.pkl', 'rb'))
+    else:
+        print('Training the model...')
+        svm_trained = knc.fit(X_train, y_train.values.ravel())
+        with open('../Data/Models/svm.pkl', 'wb') as handle:
+            pickle.dump(svm_trained, handle)
+    pred=svm_trained.predict(X_test)
     print("Confusion matrix on test set:\n", metrics.confusion_matrix(y_test, pred))
     print("\nAccuracy on test set: ", metrics.accuracy_score(y_test, pred))
     print('Full report: \n: ', metrics.classification_report(y_test, pred))
