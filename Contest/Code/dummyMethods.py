80e607f7d45edadc395efb03e4b1a2d79b5f816e
Unknown <raquelpa93@gmail.com>
2017-11-26 14:10:02 +0100

    añado lda, regresion logistica qda knn y algo de documetacion

--- Contest/Code/dummyMethods.py
+++ Contest/Code/dummyMethods.py
@@ -10,27 +10,30 @@ from sklearn.ensemble import RandomForestClassifier
 from os import path
 from Contest.Code.split_data import split
 from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+
 import pickle
 import gc
 
 
 def lda(X_train, X_test, y_train, y_test):
     """
-    La regresión logística de toda la vida.
-    La idea es hacer un clasificador lineal y pasarlo por la función logística.
-    P(C|x) = g(wTx+w0)
+    LDA
     Suposiciones:
     - Gausianidad de los datos
     - La misma varianza
+    Hiperparametros: No
     :param X_train:
     :param X_test:
     :param y_train:
     :param y_test:
     :return:
     """
-    print("Regresion Lineal:")
+    print("LDA:")
     # Importante estandarizar datos
-    lda = LogisticRegression(solver='saga', n_jobs=-1)
+    lda = LinearDiscriminantAnalysis()
     _path = '../Data/Models/lda.plk'
     if path.isfile(_path):
         lda_trained = pickle.load(open(_path, 'rb'))
@@ -42,9 +45,85 @@ def lda(X_train, X_test, y_train, y_test):
     pred = lda_trained.predict(X_test)
     print("Accuracy:", metrics.accuracy_score(y_test, pred))
     print('Full report: \n', metrics.classification_report(y_test, pred))
+    del lda, lda_trained
+    gc.collect()
+
+
+def logistic_regresion(X_train, X_test, y_train, y_test):
+    """
+    La regresión logística de toda la vida.
+    La idea es hacer un clasificador lineal y pasarlo por la función logística.
+    P(C|x) = g(wTx+w0)
+    Suposiciones:
+    -
+    Hiperparametros:
+    - No
+    :param X_train:
+    :param X_test:
+    :param y_train:
+    :param y_test:
+    :return:
+    """
+    print("Regresion Lineal:")
+    # Importante estandarizar datos
+    lr = LogisticRegression(solver='saga', n_jobs=-1)
+    _path = '../Data/Models/lr.plk'
+    if path.isfile(_path):
+        lr_trained = pickle.load(open(_path, 'rb'))
+    else:
+        print('Training the model...')
+        lr_trained = lr.fit(X_train, y_train.values.ravel())
+        with open(_path, 'wb') as handle:
+            pickle.dump(lr_trained, handle)
+    pred = lr_trained.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, pred))
+    print('Full report: \n', metrics.classification_report(y_test, pred))
+    del lr, lr_trained
+    gc.collect()
+
+
+def qda(X_train, X_test, y_train, y_test):
+    """
+    QDA
+    Suposiciones:
+    - Gausianidad de los datos
+    - Diferentes varianzas
+    Hiperparametros:
+    - No
+    :param X_train:
+    :param X_test:
+    :param y_train:
+    :param y_test:
+    :return:
+    """
+    print("QDA:")
+    # Importante estandarizar datos
+    qda = QuadraticDiscriminantAnalysis()
+    _path = '../Data/Models/qda.plk'
+    if path.isfile(_path):
+        qda_trained = pickle.load(open(_path, 'rb'))
+    else:
+        print('Training the model...')
+        qda_trained = qda.fit(X_train, y_train.values.ravel())
+        with open(_path, 'wb') as handle:
+            pickle.dump(qda_trained, handle)
+    pred = qda_trained.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, pred))
+    print('Full report: \n', metrics.classification_report(y_test, pred))
+    del qda, qda_trained
+    gc.collect()
 
 
 def naive_bayes(X_train, X_test, y_train, y_test):
+    """
+    Hiperparametros:
+    - No
+    :param X_train:
+    :param X_test:
+    :param y_train:
+    :param y_test:
+    :return:
+    """
     print('Naive Bayes: ')
     clf = GaussianNB()
     if path.isfile('../Data/Models/nb'):
@@ -58,29 +137,45 @@ def naive_bayes(X_train, X_test, y_train, y_test):
     pred = clf_trained.predict(X_test)
     print("Accuracy:", metrics.accuracy_score(y_test, pred))
     print('Full report: \n', metrics.classification_report(y_test, pred))
+    del clf, clf_trained
+    gc.collect()
 
 
-def knn(X_train, X_test, y_train, y_test):
-    print('KNN: ')
+def knn(X_train, X_test, y_train, y_test, neighbors):
+    """
+    Hiperparametros:
+    - n_neighbours
+    :param X_train:
+    :param X_test:
+    :param y_train:
+    :param y_test:
+    :param n_neighbors:
+    :return:
+    """
+    print('KNN ', neighbors, ' :')
     # Create a kNN classifier object
-    knc = nb.KNeighborsClassifier()
-    if path.isfile('../Data/Models/knn'):
-        knc_trained = pickle.load(open('../Data/Models/knn', 'rb'))
+    knc = nb.KNeighborsClassifier(n_neighbors=neighbors, n_jobs=-1)
+    _path = '../Data/Models/knn' + str(neighbors) + 'pkl'
+    if path.isfile(_path):
+        knc_trained = pickle.load(open(_path, 'rb'))
     else:
         print('Training the model...')
         # Train the classifier
         knc_trained = knc.fit(X_train, y_train.values.ravel())
-        with open('../Data/Models/knn', 'wb') as handle:
+        with open(_path, 'wb') as handle:
             pickle.dump(knc_trained, handle)
+        print('trained')
 
     pred = knc_trained.predict(X_test)
     # Obtain accuracy score of learned classifier on test data
     print('Accuracy: ', knc.score(X_test, y_test))
     print('Full report: \n: ', metrics.classification_report(y_test, pred))
+    del knc_trained, knc
+    gc.collect()
 
 
 def random_forest(X_train, X_test, y_train, y_test, n_estimators):
-    print('Random Forest: ')
+    print('Random Forest ', n_estimators,' :')
     rf_path = '../Data/Models/rf' + str(n_estimators) + '.pkl'
     rf = RandomForestClassifier(n_estimators=n_estimators)
     if path.isfile(rf_path):
@@ -98,15 +193,19 @@ def random_forest(X_train, X_test, y_train, y_test, n_estimators):
     gc.collect()
 
 
-def run_rf(X_train, X_test, X_val, y_train, y_test, y_val):
-    for i in [1, 3, 5, 7, 9, 11, 13, 15, 17]:
-        random_forest(X_train, X_test, y_train, y_test, i)
+def run_rf_knn(X_train, X_test, X_val, y_train, y_test, y_val):
+    for i in [1, 3, 5, 7, 9, 11, 13, 15, 17,19,21,23,25,27,29,31]:
+        #random_forest(X_train, X_test, y_train, y_test, i)
+        knn(X_train, X_test, y_train, y_test, i)
 
 
 def main():
-    (X_train, X_test, X_val, y_train, y_test, y_val) = split(0.95)
+    (X_train, X_test, X_val, y_train, y_test, y_val) = split(0.50)
+    naive_bayes(X_train, X_test, y_train, y_test)
     lda(X_train, X_test, y_train, y_test)
-
+    qda(X_train, X_test, y_train, y_test)
+    logistic_regresion(X_train, X_test, y_train, y_test)
+    run_rf_knn(X_train, X_test, X_val, y_train, y_test, y_val)
 
 if __name__ == "__main__":
     main()
